04d01c461a1353a35d2d903b24be375c56d1c77a
Sekunda Mkenda <sekundamkenda07@gmail.com>
2024-07-03 21:58:51 +0300

    update:urls

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,2 +1,2 @@
-const baseUrl = 'http://192.168.145.2:8000/api/';
-const nakedBaseUrl = 'http://192.168.145.2:8000';
+const baseUrl = 'http://192.168.37.235:8000/api/';
+const nakedBaseUrl = 'http://192.168.37.235:8000';
