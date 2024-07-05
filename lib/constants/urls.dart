076630430925bc2b72908358bc0bf4b7fdc93bca
meczeck <meshackmtweve13@outlook.com>
2024-07-05 09:42:59 +0300

    update:frontend

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -1,2 +1,2 @@
-const baseUrl = 'http://192.168.145.2:8000/api/';
-const nakedBaseUrl = 'http://192.168.145.2:8000';
+const baseUrl = 'http://192.168.100.71:8000/api/';
+const nakedBaseUrl = 'http://192.168.100.71:8000';
